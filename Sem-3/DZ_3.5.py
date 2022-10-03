3d22e1afebfb42aa09f3452c96143bffd63cb416
Bor_on <borzyhanton@gmail.com>
2022-10-04 01:58:06 +0300

    dz5

---
+++ Sem-3/DZ_3.5.py
@@ -0,0 +1,39 @@
+#Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+#Пример:
+# для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+def enter_user_num(text):
+    digit = 0
+    while (digit == 0):
+        digit = int(input(text))
+    return digit
+
+
+
+def fibo(n):
+    if n in [1, 2]:                       
+        return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+def n_fibo(n):
+    if n == 1:                       
+        return 1
+    elif n == 2:                       
+        return -1
+    else:
+        num1, num2 = 1, -1
+        for i in range(2, n):
+            num1, num2 = num2, num1 - num2
+        return num2
+
+def fibbo_list(n):
+    list = [0]
+    for e in range(1, user_number + 1):
+        list.append(fibo(e))
+        list.insert(0, n_fibo(e))
+    return list
+
+user_number = enter_user_num("введите число: ")
+result_list = fibbo_list(user_number)
+print(result_list)
